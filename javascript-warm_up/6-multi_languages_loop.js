223fb073c042cbf9754062ab6318c9621d76271b
m-polwayne <pholoanemotshidisi004@gmail.com>
2023-09-10 17:17:11 +0200

    again

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 const c = 1;
 
 for (let i = 0; i < c; i++) {
-  console.log("C is fun \nPython is cool \nJavaScript is amazing");
+  console.log("C is fun\nPython is cool\nJavaScript is amazing");
 }
